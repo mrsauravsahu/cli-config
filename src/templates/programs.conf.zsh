35be4ca88828e3b1895d88201d503703fa1077e3
mrsauravsahu <mrsauravsahu@outlook.com>
2026-06-07 11:09:36 +0530

    chore: remove template for conf - use direct values in generated conf

--- src/templates/programs.conf.zsh
+++
@@ -1,7 +0,0 @@
-autoload -Uz compinit && compinit
-
-# init all cli-config tools
-for tool in $(find $CLI_CONFIG_CONF_LOCATION -type f -name '*.sh' | sort); do . $tool; done
-
-# alias cls to clear
-alias cls=clear
