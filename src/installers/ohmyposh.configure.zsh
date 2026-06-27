6261ce493dba11934e180cbd475c2626c733abf3
Sahu, S <mrsauravsahu@outlook.com>
2026-06-27 14:58:34 +0530

    chore: fix <200b> character in tmux inside nvim

--- src/installers/ohmyposh.configure.zsh
+++ src/installers/ohmyposh.configure.zsh
@@ -3,3 +3,11 @@ CONF=$CLI_CONFIG_CONF_LOCATION/$TOOL.conf.sh
 
 echo -n >$CONF
 printf 'eval "$($CLI_CONFIG_ROOT/current/ohmyposh/oh-my-posh init zsh --config $CLI_CONFIG_ROOT/current/ohmyposh/themes/$CLI_CONFIG_THEME.omp.json)"\n' >>$CONF
+printf 'if [[ -n "$NVIM" ]]; then\n' >>$CONF
+printf '  _omp_strip_zwsp() {\n' >>$CONF
+printf '%s\n' "    local zwsp=\$'\\xe2\\x80\\x8b'" >>$CONF
+printf '    PROMPT="${PROMPT//$zwsp/}"\n' >>$CONF
+printf '    RPROMPT="${RPROMPT//$zwsp/}"\n' >>$CONF
+printf '  }\n' >>$CONF
+printf '  add-zsh-hook precmd _omp_strip_zwsp\n' >>$CONF
+printf 'fi\n' >>$CONF
