af557f19d02ed595e6040d965e1cf42b74cc0980
Sahu, S <mrsauravsahu@outlook.com>
2026-06-27 15:12:17 +0530

    chore: configure asdf to add it to PATH when configuring

--- src/installers/asdf.configure.zsh
+++ src/installers/asdf.configure.zsh
@@ -6,4 +6,6 @@ echo -n >$CONF
 tee $CONF >/dev/null <<'EOF'
 export ASDF_DIR="$CLI_CONFIG_ROOT/current/asdf"
 export ASDF_DATA_DIR="$CLI_CONFIG_ROOT/current/asdf"
+export PATH="$ASDF_DIR/bin:$PATH"
 EOF
+
